6329695cd458b06f145f34ea4d6bbc5701146a75
Ganguohao <32545116+logono123@users.noreply.github.com>
2018-05-29 15:43:54 +0800

    Initial commit

---
+++ Assets/Playersetup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Playersetup : NetworkBehaviour{
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    private void Start()
+    {
+        if(!isLocalPlayer)
+
+                for(int i=0; i < componentsToDisable.Length; i++)
+            {
+
+                componentsToDisable[i].enabled = false;
+            }
+
+
+    }
+
+}
---
+++ Assets/scripts/scene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;//增加命名空间
+
+public class scene : MonoBehaviour
+{
+    public void OnStartGame(string sceneName)
+    {
+        //Application.LoadLevel("menu");  // sceneName就是你要加载的场景----这是老式的用法
+        SceneManager.LoadScene(1);//1是场景的索引
+        // Application.LoadLevel(sceneName);
+    }
+}
